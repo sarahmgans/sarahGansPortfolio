89330e874bf0734ad9e1eb26562ad757a6cc27ff
sarahmgans <sarah.gans@hotmail.com>
2020-05-03 09:00:34 -0400

    fixed the nav

--- script.js
+++ script.js
@@ -1,13 +1,49 @@
 $(document).ready(function () {
-
-  scroll = function (scrollTo) {
+  const scroll = function (scrollTo) {
     $('html, body').animate({
       scrollTop: $(scrollTo).offset().top-55
-    }, 600);
+    }, 500);
   }
 
-  $('a.home, a.about, a.skills, a.projects, a.contact').on('click', function (e) {
-    e.preventDefault();
-    scroll($(this).attr('href'))
-  })
+  const clickEventStart = () => {
+    $('a.down').on('click', function (e) {
+      e.preventDefault();
+      scroll('main');
+    })
+  }
+
+  clickEventStart();
+
+  // From https://codepen.io/yuki-san/pen/eJqLNO
+  const sections = $('section')
+    nav = $('nav')
+    nav_height = 55
+
+  $(window).on('scroll', function () {
+    var cur_pos = $(this).scrollTop();
+
+    sections.each(function () {
+      var top = $(this).offset().top - nav_height,
+        bottom = top + $(this).outerHeight();
+
+      if (cur_pos >= top && cur_pos <= bottom) {
+        nav.find('a').removeClass('active');
+        sections.removeClass('active');
+
+        $(this).addClass('active');
+        nav.find('a[href="#' + $(this).attr('id') + '"]').addClass('active');
+      }
+    });
+  });
+
+  nav.find('a').on('click', function () {
+    var $el = $(this)
+        id = $el.attr('href');
+
+    $('html, body').animate({
+      scrollTop: $(id).offset().top - 55
+    }, 500);
+
+    return false;
+  });
 });
