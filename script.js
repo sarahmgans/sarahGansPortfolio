199a58e3b072776bbd9e80ec88a6f2e0b7d3a5d1
sarahmgans <sarah.gans@hotmail.com>
2020-05-12 22:33:11 -0400

    opacity

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
   const scroll = function (scrollTo) {
     $('html, body').animate({
-      scrollTop: $(scrollTo).offset().top-77
+      scrollTop: $(scrollTo).offset().top - 77
     }, 500);
   }
 
@@ -17,7 +17,7 @@ $(document).ready(function () {
   // From https://codepen.io/yuki-san/pen/eJqLNO
 
   // All of the sections besides the contact section, as the nav bar never reaches the top of the contact section so it has a different top value. 
-  let section = $('section#home, section#about, section#skills, section#projects')
+  let section = $('section#home, section#about, section#skills, section#projects, section#contact')
     let nav = $('nav')
     const nav_height = 78
 
@@ -31,24 +31,6 @@ $(document).ready(function () {
       if (cur_pos >= top && cur_pos <= bottom) {
         nav.find('a').removeClass('active');
         section.removeClass('active');
-
-        $(this).addClass('active');
-        nav.find('a[href="#' + $(this).attr('id') + '"]').addClass('active');
-      }
-    });
-  });
-
-  // The contact section has a different top value so it gets its seperate function. 
-  $(window).on('scroll', function () {
-    var cur_pos = $(this).scrollTop();
-
-    $('section.contact').each(function () {
-      var top = $(this).offset().top - 78,
-        bottom = top + $(this).outerHeight();
-
-      if (cur_pos >= top && cur_pos <= bottom) {
-        nav.find('a').removeClass('active');
-        $('section.contact').removeClass('active');
 
         $(this).addClass('active');
         nav.find('a[href="#' + $(this).attr('id') + '"]').addClass('active');
